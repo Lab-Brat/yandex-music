981abc6e6eb311ab58b8a9d6960cb8a3b26584a6
Lab-Brat <xoovoox@protonmail.com>
2022-09-17 23:24:23 +0300

    write to file

--- main.py
+++ main.py
@@ -17,21 +17,24 @@ class Music():
         sys.stdout = sys.__stdout__
         return client
 
-    def get_liked_songs(self):
+    def get_liked_songs(self, write=False):
         likes = self.client.users_likes_tracks()
         tracks = []
         for i, track in enumerate(likes):
-            print(f'track {i}, count: {(i+1) % 10}')
             info = track.fetch_track()
             tracks.append((info.title, info.artists[0]['name']))
-            if (i+1) % 10 == 0:
-                print('Read 10 tracks')
-                break
+            if (i+1) % 20 == 0:
+                print('Read 20 tracks')
+
+        if write == True:
+            self.write(tracks)
         
         return tracks
 
+    def write(self, content):
+        with open('content.txt', 'w') as f:
+            [f.write(f"Name:{line[0]}, Artist: {line[1]}\n") for line in content]
 
 if __name__ == '__main__':
     ym = Music()
-    likes = ym.get_liked_songs()
-    print(likes)
+    likes = ym.get_liked_songs(write=True)

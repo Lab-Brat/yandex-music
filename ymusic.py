4cedab58db91750f1d169f50e407535e1bc8dd24
Lab-Brat <xoovoox@protonmail.com>
2022-09-18 23:45:47 +0300

    add documentation

--- ymusic.py
+++ ymusic.py
@@ -8,16 +8,29 @@ class Music():
         self.client = self.authenticate()
 
     def get_token(self, token_file):
+        '''
+        Read API token from a text file,
+        Return token in string format.
+        '''
         with open(token_file, 'r') as f:
             return f.readlines()[0]
 
     def authenticate(self):
+        '''
+        Use token for authentication,
+        All output in this method is supressed.
+        Return authenticated client object
+        '''
         sys.stdout = open(os.devnull, 'w')
         client = Client(self.token).init()
         sys.stdout = sys.__stdout__
         return client
 
     def get_liked_songs(self, write=False):
+        '''
+        Get (name, artist) from every liked track,
+        Return a list of tuples
+        '''
         likes = self.client.users_likes_tracks()
         tracks = []
         for i, track in enumerate(likes):
@@ -32,8 +45,12 @@ class Music():
         return tracks
 
     def write(self, content):
+        '''
+        Write information about liked songs to a text file
+        '''
         with open('content.txt', 'w') as f:
             [f.write(f"Name:{line[0]}, Artist: {line[1]}\n") for line in content]
+
 
 if __name__ == '__main__':
     ym = Music()
